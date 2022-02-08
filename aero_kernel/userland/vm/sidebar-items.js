668353654abd7caa3a7205b90a9998b2b4420076
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2022-02-08 07:25:46 +0000

    deploy: 5738b7ad5abd60cc2c0df1f23fd82629c1c9892c

--- aero_kernel/userland/vm/sidebar-items.js
+++ aero_kernel/userland/vm/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["UnmapResult",""]],"struct":[["LoadedBinary",""],["MMapFile",""],["Mapping",""],["Vm",""],["VmProtected",""]]});
+initSidebarItems({"constant":[["ELF_HEADER_MAGIC",""],["ELF_PT1_SIZE",""],["ELF_PT2_64_SIZE",""]],"enum":[["ElfParseError",""],["UnmapResult",""]],"fn":[["parse_elf_header",""],["parse_program_header",""]],"struct":[["LoadedBinary",""],["MMapFile",""],["Mapping",""],["ProgramHeaderIter",""],["Vm",""],["VmProtected",""]]});
