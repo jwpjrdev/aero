4bc4e5f2b610428abb74bc0b7cb77ef72aa090c0
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2022-06-24 07:11:17 +0000

    deploy: c7dfb7775661e22a210664510ada1592cd8c0e5b

---
+++ aero_kernel/syscall/futex/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["get_futex_conatiner","Returns a reference to the futex conatiner; initializing if necessary."],["wait",""],["wake",""]],"static":[["FUTEX_CONTAINER",""]],"struct":[["FutexContainer",""]]};
--- aero_kernel/syscall/sidebar-items.js
+++ aero_kernel/syscall/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["exec_args_from_slice",""],["generic_do_syscall",""]],"mod":[["fs",""],["ipc",""],["net",""],["process",""],["time",""]],"struct":[["ExecArgs",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["exec_args_from_slice",""],["generic_do_syscall",""]],"mod":[["fs",""],["futex",""],["ipc",""],["net",""],["process",""],["time",""]],"struct":[["ExecArgs",""]]};
